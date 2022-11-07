93f629268cbf0398c76d0a42d10080bbd68c4207
kazmi <costellationoblivion@gmail.com>
2022-11-08 01:42:37 +0200

    Добавьте файлы проекта.

---
+++ Ex1/Ex1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Ex1
+{
+    static void Main()
+    {
+        string pattern = @"\b[A-Z]{1}[a-z]*\s[A-Z]{1}[a-z]*\b"; // шаблон
+        /*
+            \b    границя слова, позиція між \w і \W, або між \w і початком строки
+            [A-Z]{1}    перша буква велика і її кількість 1
+            [a-z]*    подальші букти лише в малому регістрі і кількість обмежена до кінця строки або пробіла
+            \s    пробіл
+            [A-Z]{1}    перша буква після пробіла велика і її кількість 1
+            [a-z]*    подальші букти лише в малому регістрі і кількість обмежена до кінця строки або пробіла
+            \b    границя слова, позиція між \w і \W, або між \w і початком строки
+        */
+
+        while (true)
+        {
+            string? action = Console.ReadLine();
+
+
+            if (action?.ToLower() == "end") break;
+
+            if (action != null)
+                if (Regex.IsMatch(action, pattern))
+                {
+                    // візуально змінюється вивід
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine(action);
+
+                    // візуально змінюється вивід назад
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+        }
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Ex2/Ex2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Ex2
+{
+    static void Main()
+    {
+        string pattern = @"+(359)\s2\s[0-9]{3}\s[0-9]{4}\b"; // шаблон
+        /*
+         * 
+        */
+
+        while (true)
+        {
+            string? action = Console.ReadLine();
+
+
+            if (action?.ToLower() == "end") break;
+
+            if (action != null)
+                if (Regex.IsMatch(action, pattern))
+                {
+                    // візуально змінюється вивід
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine(action);
+
+                    // візуально змінюється вивід назад
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+        }
+
+
+        Console.ReadKey();
+    }
+}
