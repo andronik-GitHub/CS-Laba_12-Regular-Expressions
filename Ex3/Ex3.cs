f36932dc2c2151bfe6634a8d57874aa1d72e49ad
kazmi <costellationoblivion@gmail.com>
2022-11-08 01:42:37 +0200

    Добавьте файлы проекта.

---
+++ Ex3/Ex3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Ex3
+{
+    static void Main()
+    {
+        string? action = "aaaaabbbbbcdddeeeedssaa"; //Console.ReadLine();
+
+        if (action != null)
+        {
+            string pattern = @"(\w)\1+";
+
+            /*
+               (   група 1
+                    \w   яка складається з одної букви
+                )
+                \1+   повторення групи 1 один раз і більше
+             */
+
+            Regex regex = new(pattern);
+            action = regex.Replace( action,
+                                    "$1" // заміна групою 1
+                                  );
+
+            Console.WriteLine(action);
+        }
+
+
+        Console.ReadKey();
+    }
+}
